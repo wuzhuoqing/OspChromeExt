c8eb6a2b1dcbb7fff49d687aecbc009ad8eedc79
Zhuoqing <wuzhuoqing@hotmail.com>
2023-12-21 22:51:37 -0800

    remove zip step in webpack

--- utils/build.js
+++ utils/build.js
@@ -6,38 +6,25 @@ process.env.ASSET_PATH = '/';
 var webpack = require('webpack'),
   path = require('path'),
   fs = require('fs'),
-  config = require('../webpack.config'),
-  ZipPlugin = require('zip-webpack-plugin');
+  config = require('../webpack.config')
 
 delete config.chromeExtensionBoilerplate;
 
 config.mode = 'production';
 
-var packageInfo = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
-
-config.plugins = (config.plugins || []).concat(
-  new ZipPlugin({
-    filename: `${packageInfo.name}-${packageInfo.version}.zip`,
-    path: path.join(__dirname, '../', 'zip'),
-  })
-);
-
 var buildFolder = path.join(__dirname, '..', 'build');
 
 console.log('checking folder', buildFolder);
 
-if (!fs.existsSync(buildFolder)) {
-  console.log('creating folder', buildFolder);
-  fs.mkdirSync(buildFolder)
-}
-
-console.log('before final webpack', __dirname);
+console.log('before final webpack', __dirname, config);
 
 try {
   webpack(config, function (err) {
     if (err) {
       console.error('failed to run webpack', err);
       throw err;
+    } else {
+      console.log('end of webpack run');
     }
   });
 }
